429f3b367cb1e3ee0f92b674a712b054aa0ba3c4
[vedhashni] <[vedhavel.1234@gmail.com]>
2021-07-01 12:52:35 +0530

    [Vedhashni] Updated the file as Welcome To Basic C# and Functional Problems

---
+++ Basic_Functional_Problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Functional_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to basic C# and functional problems!");
+        }
+    }
+}
